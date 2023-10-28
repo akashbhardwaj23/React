8bdf2382af31e43b8deaf85a6b932ec206f24e11
akash23-ai <akashbhardwaj2024@gmail.com>
2023-10-28 18:50:20 +0530

    Change in 02Counter File

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -6,8 +6,16 @@ function App() {
   const [counter , setCounter] = useState(15)
 //  let counter = 15;
  const addValue = ()=>{
-   setCounter(prevcounter => prevcounter+1);
-   setCounter(prevcounter => prevcounter+1);
+  if (counter < 20) {
+    setCounter(counter + 1)
+  }
+   
+
+
+   // ************** Will Work On this again ****************************8
+
+
+  //  setCounter(prevcounter => prevcounter+1);
   //  setCounter((prevCounter) => {
   //   prevCounter+1;
   //  });
@@ -15,23 +23,33 @@ function App() {
   //   prevCounter+1;
   //  });
  }
- const removeValue = ()=>{
-  setCounter(counter - 1);
+ const RemoveValue = ()=>{
+  if (counter <= 0) {
+    console.log('Please Add')
+    // *************************Will Update this Later*************************
+    // return (                            
+    //     <h3> Please Add </h3>
+    //      );
+  } else {
+    setCounter(counter - 1)
+  }
+  
  }
-
-
 
  function reset (){
   setCounter(15)
  }
+
+
   return (
     <>
       <h1>React Course {counter} </h1>
       <h2>Counter value : {counter} </h2>
       <button onClick={addValue}>Add Button</button> {" "}
-      <button onClick={removeValue}>Remove Value</button> {" "}
+      <button onClick={RemoveValue}>Remove Value</button> {" "}
       <button onClick={reset}>Reset</button>
       <p>footer: {counter} </p>
+      
     </>
   )
 }
